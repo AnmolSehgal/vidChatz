81c894974a4bd12a9d4f2bbab0db422807d35c99
Anmol Sehgal <anmolsehgal@Anmols-MacBook-Air.local>
2023-03-28 01:59:52 +0530

    chore: installing dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ],
+    'import/no-unresolved': 'error',
+    'import/no-named-as-default': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  }
+};
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import react from 'react';
+function App() {
+  return <></>;
+}
+
+export default App;
